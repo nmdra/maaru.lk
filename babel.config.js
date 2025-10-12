86906afb8a11873b2a6cce274ae83aace775da17
lksnjw <lk.snjw@gmail.com>
2025-10-12 09:30:14 +0530

    search fixed
    build: update
    
    build: update

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel'
+    ],
     plugins: [
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-worklets/plugin',
+      'react-native-worklets/plugin',    // ✅ required by expo-router
+      'react-native-reanimated/plugin'   // ✅ must be last
     ],
   };
 };
