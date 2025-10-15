fe231fb785a9546f6a913e746985761977c224d5
LakshanSanjeewa <lk.snjw@gmail.com>
2025-10-16 00:00:05 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-const path = require('path');
+// const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -8,14 +8,14 @@ const config = getDefaultConfig(__dirname);
 config.resetCache = true;
 
 // Mock react-native-keyboard-controller for Expo Go compatibility (optional)
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === 'react-native-keyboard-controller') {
-    return {
-      filePath: path.resolve(__dirname, 'react-native-keyboard-controller-mock.js'),
-      type: 'sourceFile',
-    };
-  }
-  return context.resolveRequest(context, moduleName, platform);
-}; 
+// config.resolver.resolveRequest = (context, moduleName, platform) => {
+//   if (moduleName === 'react-native-keyboard-controller') {
+//     return {
+//       filePath: path.resolve(__dirname, 'react-native-keyboard-controller-mock.js'),
+//       type: 'sourceFile',
+//     };
+//   }
+//   return context.resolveRequest(context, moduleName, platform);
+// }; 
 
 module.exports = withNativeWind(config, { input: './global.css' });
