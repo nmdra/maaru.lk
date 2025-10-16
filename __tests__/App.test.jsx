b61f98d4f07b11d120383797cf5a1436f60d1f2b
Gayashan <hansagayashan7@gmail.com>
2025-10-16 20:21:54 +0530

    test render app without crashing

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
@@ -6,4 +5,9 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders app without crashing', () => {
+  render(<App />);
+  expect(screen.getByRole('main')).toBeInTheDocument();
 });
